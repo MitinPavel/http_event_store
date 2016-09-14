819777b7020f5d2cba218d1819710fcf0868189f
Pavlo Mitin <mitin.pavel@gmail.com>
2016-09-14 07:59:21 +0100

    Experiment with batch appending to a stream

--- src/client.rs
+++ src/client.rs
@@ -9,6 +9,10 @@ impl Client {
         Client { api: Api {} }
     }
 
+    pub fn append_to_stream(&self, stream_name: String, expectedVersion: u64, events: String) {
+        self.api.append_to_stream(stream_name, expectedVersion, events)
+    }
+
     pub fn get(&self) {
         self.api.get()
     }
--- tests/test_smoke/mod.rs
+++ tests/test_smoke/mod.rs
@@ -3,10 +3,29 @@ extern crate http_event_store as es;
 
 #[test]
 fn it_interacts_with_event_store() {
-  let client = es::client::Client::new();
+    let client = es::client::Client::new();
 
-  client.get();
+    // raw string literal
+    let raw_json = r#"[
+                     {
+                       "eventId": "50ed34a2-b26e-4610-8a2e-35ae8e63599e",
+                       "eventType": "task-created",
+                       "data": {
+                         "name": "Initial Name"
+                       }
+                     },
+                     {
+                       "eventId": "26e9fdd6-d7fe-4acf-b8dc-a494300963dc",
+                       "eventType": "task-renamed",
+                       "data": {
+                         "name": "Updated Name"
+                       }
+                     }
+                   ]"#;
+
+    client.append_to_stream("task-123".into(), 987, raw_json.into());
+
+//    client.get();
 
     println!("Hello")
-
 }
