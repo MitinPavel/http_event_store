e3c490dcad3f7b6b54f4da5cee0604b28ebcfbbe
Pavlo Mitin <mitin.pavel@gmail.com>
2016-09-15 08:29:51 +0100

    Use a dedicated stream name for each test run

--- tests/test_smoke/mod.rs
+++ tests/test_smoke/mod.rs
@@ -1,3 +1,4 @@
+extern crate time;
 extern crate http_event_store as es;
 
 
@@ -23,9 +24,14 @@ fn it_interacts_with_event_store() {
                      }
                    ]"#;
 
-    client.append_to_stream("task-123".into(), 987, raw_json.into());
+    let stream_name = test_stream_name();
+    client.append_to_stream(stream_name, 987, raw_json.into());
 
 //    client.get();
 
     println!("Hello")
+}
+
+fn test_stream_name() -> String {
+ format!("task-{}", time::get_time().sec)
 }
