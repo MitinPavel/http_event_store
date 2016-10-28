ff3c97be41eb6acd74683cbf8ee19710066dc525
Pavlo Mitin <mitin.pavel@gmail.com>
2016-10-28 07:42:35 +0100

    Do refactoring
    ExpectedVersion::NoConflict was renamed to ExpectedVersion::Any according to github.com/EventStore/EventStore codebase

--- src/expected_version.rs
+++ src/expected_version.rs
@@ -3,7 +3,7 @@ pub enum ExpectedVersion {
     Number(u64),
     Empty,       //  0 states that the stream should exist but should be empty.
     NoStream,    // -1 states that the stream should not exist at the time of the writing (this write will create it).
-    NoConflict,  // -2 states that this write should never conflict with anything and should always succeed.
+    Any,         // -2 states that this write should never conflict with anything and should always succeed.
     Exist,       // -4 to say that the stream should exist with any number of events in it.
 
     Unexpected(String) // Any string (including -3 OR less than or equal -5).
@@ -15,7 +15,7 @@ impl From<ExpectedVersion> for String {
             ExpectedVersion::Number(n) => n.to_string(),
             ExpectedVersion::Empty => "0".to_string(),
             ExpectedVersion::NoStream => "-1".to_string(),
-            ExpectedVersion::NoConflict => "-2".to_string(),
+            ExpectedVersion::Any => "-2".to_string(),
             ExpectedVersion::Exist => "-4".to_string(),
             ExpectedVersion::Unexpected(s) => s,
         }
@@ -26,7 +26,7 @@ impl From<String> for ExpectedVersion {
     fn from(string: String) -> ExpectedVersion {
         match string.as_ref() {
             "-4" => ExpectedVersion::Exist,
-            "-2" => ExpectedVersion::NoConflict,
+            "-2" => ExpectedVersion::Any,
             "-1" => ExpectedVersion::NoStream,
             "0" => ExpectedVersion::Empty,
             _ => ExpectedVersion::parse_number_or_unexpected(string)
