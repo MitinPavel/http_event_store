c55e962f6bd12b122da6c15ba9d5674c898a55ab
Pavlo Mitin <mitin.pavel@gmail.com>
2016-10-28 07:27:34 +0100

    Start test functions with `should_`

--- tests/test_append_to_stream.rs
+++ tests/test_append_to_stream.rs
@@ -16,7 +16,7 @@ use hes::error::HesError::*;
 use hes::error::UserErrorKind::*;
 
 #[test]
-fn it_appends_events_in_right_order() {
+fn should_append_events_in_right_order() {
     let events: Vec<Event> = vec![
         TaskCreated { name: format!("Created {:?}", time::get_time()), event_id: uuid::Uuid::parse_str("baca1a30-b6f1-470b-b68e-f79338020327").unwrap() }.into(),
         TaskRenamed { name: format!("Renamed {:?}", time::get_time()), event_id: uuid::Uuid::parse_str("cbad187b-2fd0-4ad2-b78b-80d83f1ff303").unwrap() }.into()
@@ -34,7 +34,7 @@ fn it_appends_events_in_right_order() {
 }
 
 #[test]
-fn it_requires_expected_version_to_be_correct() {
+fn should_require_expected_version_to_be_correct() {
     let client = Client::default();
     let stream_name = test_stream_name();
 
@@ -49,12 +49,10 @@ fn it_requires_expected_version_to_be_correct() {
     version = ExpectedVersion::Number(1);
     client.append_to_stream(&stream_name, version, vec![task_renamed_event().into()]).unwrap();
     assert_eq!(3, client.read_stream_events_forward(&stream_name, 0, 3, true).unwrap().entries.len());
-
-    println!("{:?}", client.read_stream_events_forward(&stream_name, 0, 3, true).unwrap().entries)
 }
 
 #[test]
-fn it_returns_event_number_mismatch_error_if_expected_version_is_wrong() {
+fn should_return_event_number_mismatch_error_if_expected_version_is_wrong() {
     let client = Client::default();
     let stream_name = test_stream_name();
 
@@ -65,7 +63,7 @@ fn it_returns_event_number_mismatch_error_if_expected_version_is_wrong() {
 }
 
 #[test]
-fn it_returns_bad_request_error_if_event_data_is_malformed() {
+fn should_return_bad_request_error_if_event_data_is_malformed() {
     let client = Client::default();
     let stream_name = test_stream_name();
 
@@ -78,7 +76,6 @@ fn it_returns_bad_request_error_if_event_data_is_malformed() {
 
     assert_error!(UserError, BadRequest(..), result.unwrap_err());
 }
-
 
 fn test_stream_name() -> String {
     format!("task-{}", uuid::Uuid::new_v4().simple())
--- tests/test_read_stream_events_forward.rs
+++ tests/test_read_stream_events_forward.rs
@@ -8,9 +8,9 @@ use es::error::UserErrorKind::*;
 mod support;
 
 #[test]
-fn attempt_to_read_nonexistent_stream() {
+fn should_retrun_stream_not_found_error_attempting_to_read_nonexistent_stream() {
     let client = es::client::Client::default();
-    let nonexistent_stream_name = "nonexistent";
+    let nonexistent_stream_name = "some-nonexistent";
     let result = client.read_stream_events_forward(&nonexistent_stream_name, 0, 1, true);
 
     assert_error!(UserError, StreamNotFound, result.unwrap_err());
